f10b286c438d3280eb8b98f2a18b353579780dd2
Frank <frank@61924.nl>
2011-06-04 11:23:40 +0200

    Added optimize action to rakefile.rb.

--- rakefile.rb
+++ rakefile.rb
@@ -78,6 +78,15 @@ task :generate => [:cloud, :tags] do
     sh 'jekyll'
 end
 
+# Optimze/Minimize CSS and JS
+# -------------
+task :optimize do
+    sh 'yuicompressor --type css ./style/screen-uncompressed.css >> ./style/screen.css'
+    sh 'yuicompressor --type js ./js/jquery-1.6.1.js >> ./js/all.js'
+    sh 'yuicompressor --type js ./js/history.js >> ./js/all.js'
+    sh 'yuicompressor --type js ./js/jsimgbox2.js >> ./js/all.js'
+end
+
 # Serve website
 # -------------
 task :serve do
